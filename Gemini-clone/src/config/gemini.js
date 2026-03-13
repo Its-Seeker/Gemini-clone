26f564239bdd42dbd1eebef30ff7967030a9dd64
Its-Seeker <ketanarora482@gmai.com>
2026-03-13 22:11:28 +0530

    Updated

--- Gemini-clone/src/config/gemini.js
+++ Gemini-clone/src/config/gemini.js
@@ -1,44 +1,19 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
+const API_KEY = "AIzaSyC2IKK4EMPyLJhy-RCP62g9ZSVCP2DA0RI";
 
-
-import {
-  GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
-} from "@google/generative-ai";
-
-const MODEL_NAME = "gemini-1.5-flash"; // Ya "gemini-1.0-pro" use karein
-const API_KEY = "AIzaSyDCTt9Qpql1nKPsErJPCmnyblGBwFIyL74"; // Image wali key secret rakhein
+const genAI = new GoogleGenerativeAI(API_KEY);
 
 async function runChat(prompt) {
-  const genAI = new GoogleGenerativeAI(API_KEY);
-  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
-  const generationConfig = {
-    temperature: 0.9,
-    topK: 1,
-    topP: 1,
-    maxOutputTokens: 2048,
-  };
-
-  const safetySettings = [
-    {
-      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    // Aap aur bhi safety settings yahan add kar sakte hain
-  ];
-
-  const chat = model.startChat({
-    generationConfig,
-    safetySettings,
-    history: [],
+  const model = genAI.getGenerativeModel({
+    model: "gemini-3-flash-preview"
   });
 
-  const result = await chat.sendMessage(prompt);
-  const response = result.response;
-  console.log(response.text());
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+
+  return response.text();
 }
 
-// Function ko test karne ke liye:
 export default runChat;
--- Gemini-clone/src/context/Context.jsx
+++ Gemini-clone/src/context/Context.jsx
@@ -1,28 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from "../config/gemini";
-
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
 
-
-    const onSent = async (prompt) => {
-        await runChat(prompt)
+  const onSent = async (prompt) => {
+    const[input,setinput] = useState("");
+    const[recentPrompt,setrecentPrompt] = useState("");
+    const [prevPrompts,setPrevPrompts] = useState([]);
+    const[showResult,setShowResult] = useState(false);
+    const [loading,setLoading] = useState(false);
+    const[resultData,setResultData] = useState("");
+    try {
+      const response = await runChat(prompt);
+      console.log(response);
+    } catch (error) {
+      console.error("Error:", error);
     }
+  };
+    // onSent ("what is react js")
+  const contextValue = {
+    
+  };
 
-    onSent("What is react js")
-
-    const contextValue = {
-
-    }
-
-    return (
-        <Context.Provider value={contextValue}>
-            {props.children}
-        </Context.Provider>
-    )
-}
-
+  return (
+    <Context.Provider value={contextValue}>
+      {props.children}
+    </Context.Provider>
+  );
+};
 
 export default ContextProvider;
