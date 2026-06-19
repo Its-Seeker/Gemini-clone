80fedc935b4d344abe9fbf63b67a6dcbb70698a3
Its-Seeker <ketanarora482@gmai.com>
2026-06-20 01:19:42 +0530

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
   return (
     <div className='main'>
         <div className="nav">
-            <p>Gemini</p>
+            <p>Google Gemini</p>
             <img src={assets.user_icon} alt="" />
         </div>
       <div className="main-container">
